5ed1a2de513741c6c07cd2b2676b1262c52d1430
satyamshete <shetesatyam99@gmail.com>
2022-10-03 23:38:19 +0530

    [Satyam] Employee_Wage_Part_Full_Time

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -9,3 +9,38 @@ if (empCheck == isFullTime) {
 } else {
   console.log("UC1-> Employee is Absent");
 }
+
+//UC2
+function CheckFullPartTime() {
+    return Math.floor((Math.random() * 10) % 3);
+  }
+  
+  empCheck = CheckFullPartTime();
+  const isPartTime = 2;
+  const wagePerHour = 20;
+  let empHrs;
+  let dailyWage;
+  
+  switch (empCheck) {
+    case isFullTime:
+      empHrs = 8;
+      dailyWage = wagePerHour * empHrs;
+  
+      console.log(
+        "UC2-> Employee is present for full time and his salary is: " + dailyWage);
+      break;
+  
+    case isPartTime:
+      empHrs = 4;
+      dailyWage = wagePerHour * empHrs;
+  
+      console.log(
+        "UC2-> Employee is present for Part time and his salary is: " + dailyWage);
+      break;
+  
+    default:
+      empHrs = 0;
+      dailyWage = wagePerHour * empHrs;
+      console.log("UC2-> Employee is absent and his salary is: " + dailyWage);
+      break;
+  }
