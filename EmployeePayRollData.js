ee41d950a2809e4faba1878bba9f8bc770067772
satyamshete <shetesatyam99@gmail.com>
2022-10-07 00:15:13 +0530

    [Satyam] UC15_Employee_Pay_Roll_Setter_Conditions

--- EmployeePayRollData.js
+++ EmployeePayRollData.js
@@ -18,7 +18,44 @@ class EmployeePayroll {
           this._name = name;
         } else
           throw "Check if name is valid i.e start with Capital and min 3 alphabets";
+    }
+    get id() {
+        return this._id;
       }
+    
+      set id(id) {
+        if (id > 0) {
+          this._id = id;
+        } else throw "Invalid ID , should be greater than zero";
+      }
+      get salary() {
+        return this._salary;
+      }
+    
+      set salary(salary) {
+        if (salary > 0) {
+          this._salary = salary;
+        } else throw "Invalid salary , should be greater than zero";
+      }
+      get gender() {
+        return this._gender;
+      }
+    
+      set gender(gen) {
+        if (gen == "M" || gen == "F") {
+          this._gender = gen;
+        } else throw "Gender should be M or F";
+      }
+      get date() {
+        return this._date;
+      }
+    
+      set date(date) {
+        if (date <= new Date()) {
+          this._date = date;
+        } else throw "Invalid date ,it should not be in future";
+      }
+
   
     toString() {
       return (
@@ -38,13 +75,17 @@ class EmployeePayroll {
   }
   
   try {
-    let employeeOne = new EmployeePayroll(1504, "Lisa", 2587469);
-  console.log(employeeOne);
+    let employeeOne = new EmployeePayroll(1740, "Smriti", 25874,"F",new Date());
+  console.log(employeeOne.toString());
   employeeOne.name = "Dinesh";
   console.log(employeeOne.toString());
-  let employeeSecond = new EmployeePayroll(1740, "Smriti", 25874,"F",new Date());
-  console.log(employeeSecond.toString());
-  let employeeThird = new EmployeePayroll(1650, "vinod", 74564,"M",new Date());
+  //let employeeSecond = new EmployeePayroll(0, "Vinod", 74564,"M",new Date());
+  //let employeeThird = new EmployeePayroll(1650, "chandra", 74564,"M",new Date());
+ // let employeeFourth = new EmployeePayroll(1450, "Dewanshu", 0,"M",new Date())
+  //et employeeFifth = new EmployeePayroll(950, "Poorva", 45155,"k",new Date())
+  //let employeeSix = new EmployeePayroll(750, "Deepa", 45155,"M",  new Date("Oct 24, 2022"))
+
   } catch (error) {
     console.log(error);
   }
+
