dd0f7eb130276d31e8a55c626ab6db1a31638da6
satyamshete <shetesatyam99@gmail.com>
2022-10-06 23:34:59 +0530

    [Satyam] Employee_Pay_Roll_Name_Regex_Class

--- EmployeePayRollData.js
+++ EmployeePayRollData.js
@@ -12,8 +12,13 @@ class EmployeePayroll {
     }
   
     set name(name) {
-      this._name = name;
-    }
+        //name check using Regex
+        let nameRegex = RegExp("^[A-Z]{1}[a-z]{2,}$");
+        if (nameRegex.test(name)) {
+          this._name = name;
+        } else
+          throw "Check if name is valid i.e start with Capital and min 3 alphabets";
+      }
   
     toString() {
       return (
@@ -32,10 +37,14 @@ class EmployeePayroll {
     }
   }
   
-  let employeeOne = new EmployeePayroll(1504, "Lisa", 2587469);
+  try {
+    let employeeOne = new EmployeePayroll(1504, "Lisa", 2587469);
   console.log(employeeOne);
-  
   employeeOne.name = "Dinesh";
-  console.log(employeeOne);
+  console.log(employeeOne.toString());
   let employeeSecond = new EmployeePayroll(1740, "Smriti", 25874,"F",new Date());
   console.log(employeeSecond.toString());
+  let employeeThird = new EmployeePayroll(1650, "vinod", 74564,"M",new Date());
+  } catch (error) {
+    console.log(error);
+  }
